c1d91f5a3dcae62f52cb92854aa8990effc7425c
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-09-21 14:41:20 +0900

    create image component

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,11 @@
-import { PageComponent } from './components/page.js';
+import { PageComponent } from './components/page/page.js';
+import { ImageComonent } from './components/page/item/image.js';
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
+        const image = new ImageComonent('Image Title', 'https://picsum.photos/600/300');
+        image.attachTo(appRoot, 'beforeend');
     }
 }
 new App(document.querySelector('.document'));
