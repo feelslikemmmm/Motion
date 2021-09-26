d89a3e76e9d814bafc82e359c5665270926728f9
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-09-26 17:01:07 +0900

    video REGEX

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -10,11 +10,24 @@ export class VideoComponent extends BaseComponent<HTMLLIElement> {
       '.video__iframe'
     )! as HTMLIFrameElement;
     console.log(url);
-    iframe.src = 'https://www.youtube.com/embed/K3-jG52XwuQ'; // url -> videoId
+    iframe.src = this.convertToEmbeddedURL(url);
 
     const titleElement = this.element.querySelector(
       '.video__title'
     )! as HTMLHeadingElement;
     titleElement.textContent = title;
+  }
+
+  private convertToEmbeddedURL(url: string): string {
+    // const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9(-|_)]{11}))|(?:youtu.be\/([a-zA-Z0-9(-|_)]{11})))/;
+    // const match = url.match(regExp);
+    const splited = url.split('=');
+    const videoId = splited[splited.length - 1];
+    return `https://www.youtube.com/embed/${videoId}`;
+    // const videoId = match ? match[1] || match[2] : undefined;
+    // if (videoId) {
+
+    // }
+    // return url;
   }
 }
